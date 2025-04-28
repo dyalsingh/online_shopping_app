61c9491c3ac1b3b451437bc90f5f97bd71d96f5e
Ubuntu <ubuntu@ip-172-31-13-159.us-east-2.compute.internal>
2025-04-28 09:39:30 +0000

    finally done

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-<< info 
+<< info
 ./install.sh <Name>  --> For installation
-info 
+info
 
 echo "-------Installing $1 -------"
 sudo apt-get update > /dev/null
-sudo apt-get install $1 -y > /dev/null
-$1 --version
+
+if [ "$1" == "docker-compose" ]; then
+ 
+    sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r .tag_name)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    
+    sudo chmod +x /usr/local/bin/docker-compose
+    
+    docker-compose --version
+else
+  sudo apt-get install $1 -y > /dev/null
+  $1 --version
+fi
+
 echo "-------Installation Completed------"
